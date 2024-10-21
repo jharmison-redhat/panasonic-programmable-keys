f10d9e0ea2a2e24172411cbffd92dd1b17ceb996
James Harmison <jharmison@redhat.com>
2024-10-21 17:14:44 -0400

    fix: consistent tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -18,4 +18,4 @@ def test_valid_device_parsing(valid_device):
 @pytest.mark.parametrize("invalid_device", invalid_devices)
 def test_invalid_device_parsing(invalid_device):
     with pytest.raises(ValidationError):
-        InputDevices.load(invalid_device)
+        InputDevices.load(source=invalid_device)
